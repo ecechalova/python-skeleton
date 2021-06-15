ab1be9237cabac91847390b4fdc74ec8433c3aed
Tomas Cechal <tcechal@globalwebindex.net>
2021-06-15 08:56:59 +0200

    Debug #7

--- .github/create-ci-dockerfiles.sh
+++ .github/create-ci-dockerfiles.sh
@@ -2,13 +2,16 @@
 set -ex
 
 # It's not possible to target build stages in GitHub actions `docker` action
-# so we have to template these instead to avoid code duplication.
+# so we have to template these instead to avoid code duplication. Also, the file
+# must be named "Dockerfile" which conflicts with the existing Dockerfile
 
 PROJECT_ROOT="$(dirname $(dirname $(realpath $0)))"
 
+# Save the original dockerfile
+mv "$PROJECT_ROOT/Dockerfile" "$PROJECT_ROOT/.dockerfile"
+
 for SCRIPT in "tests" "lint"
 do
-    DOCKERFILE="$PROJECT_ROOT/Dockerfile"
-    cat $PROJECT_ROOT/Dockerfile > "$DOCKERFILE"
-    printf "\nENTRYPOINT [\"/usr/src/app/scripts/$SCRIPT.sh\"]" >> "$DOCKERFILE"
+    cat "$PROJECT_ROOT/.dockerfile" > "$PROJECT_ROOT/Dockerfile"
+    printf "\nENTRYPOINT [\"/usr/src/app/scripts/$SCRIPT.sh\"]" >> "$PROJECT_ROOT/Dockerfile"
 done
